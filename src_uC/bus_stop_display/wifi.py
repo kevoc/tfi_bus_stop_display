2d30e8c9645a7cbb6ea4e7355efe34f03fa4a2ad
kevoc <kevoc@users.noreply.github.com>
2025-03-30 21:02:18 +0200

    Updates to wifi controller

--- src_uC/bus_stop_display/wifi.py
+++ src_uC/bus_stop_display/wifi.py
@@ -11,8 +11,11 @@ Wifi connection and network scans.
 
 
 import time
+import json
 import network
 from micropython import const
+
+from . import log
 
 
 SECURITY = {
@@ -27,22 +30,18 @@ SECURITY = {
 class WifiController:
     """A wifi controller to connect to and manage wifi."""
 
-    def __init__(self, network: str, password: str,
-                 connect_timeout: int = 10, logger=None):
+    def __init__(self, network_name: str, password: str,
+                 connect_timeout: int = 10):
 
-        self.network = network
-        self.password = password
-        self.connect_timeout = connect_timeout
+        self._network = network_name
+        self._password = password
+        self._connect_timeout = connect_timeout
 
         self.wlan = network.WLAN(network.WLAN.IF_STA)
         self.wlan.active(True)
 
-        self._log = logger
-
     def network_scan(self):
         """Run a network scan, logging the results."""
-
-        log = self._log  # for speed
 
         start = time.ticks_ms()
 
@@ -51,21 +50,21 @@ class WifiController:
 
             bssid_str = ":".join([f"{b:02X}" for b in bssid])
 
-            log.info(json={'SSID': ssid, 'BSSID': bssid_str,
-                           'Channel': channel, 'RSSI': RSSI,
-                           'Security': SECURITY.get(security, security),
-                           'Hidden': hidden})
+            log.info(json.dumps(
+                {'SSID': ssid, 'BSSID': bssid_str, 'Channel': channel,
+                 'RSSI': RSSI, 'Security': SECURITY.get(security, security),
+                 'Hidden': hidden}))
 
         log.info(f'Scan took {1.0e-3 * (time.ticks_ms() - start):.2f} secs.')
 
     def connect(self):
         """Connect to the network loaded from the config file."""
 
-        self._log.info(f'Connecting to network "{self.network}" with timeout of {self.connect_timeout} secs.')
-        self.wlan.connect(self.network, self.password)
+        log.info(f'Connecting to network "{self._network}" with timeout of {self._connect_timeout} secs.')
+        self.wlan.connect(self._network, self._password)
 
         start = time.ticks_ms()
-        while (time.ticks_ms() - start) < (1000 * self.connect_timeout):
+        while (time.ticks_ms() - start) < (1000 * self._connect_timeout):
             if self.wlan_status != 'connecting':
                 break
 
@@ -73,14 +72,14 @@ class WifiController:
 
         status = self.wlan_status
         if status == 'connecting':
-            self._log.error(f'timeout waiting for network connection')
+            log.error(f'timeout waiting for network connection')
             raise RuntimeError('timeout while connecting')
         elif self.wlan_status != 'got IP':
             raise RuntimeError(self.wlan_status)
         elif not self.wlan.isconnected():
             raise RuntimeError('unspecified error occurred')
 
-        self._log.info(f'successful connection after {1.0e-3 * (time.ticks_ms() - start):.2f} secs.')
+        log.info(f'successful connection after {1.0e-3 * (time.ticks_ms() - start):.2f} secs.')
 
         self.log_ip_address()
 
@@ -103,12 +102,12 @@ class WifiController:
         elif status == network.STAT_GOT_IP:
             return 'got IP'
         else:
-            return 'unrecognised status'
+            return f'unrecognised status: {status}'
 
     def log_ip_address(self):
         """Dump the IP address information to the log."""
 
         ip, subnet, gateway, dns = self.wlan.ifconfig()
 
-        self._log.info(json={'ip': ip, 'subnet': subnet,
-                             'gateway': gateway, 'dns': dns})
+        log.info(json.dumps({'ip': ip, 'subnet': subnet,
+                             'gateway': gateway, 'dns': dns}))
